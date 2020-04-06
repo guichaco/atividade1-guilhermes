47ac7d5137fba6f5e070280efc4ed8e2bdd42941
Guilherme campos <Guilherme Campos>
2020-04-06 18:39:47 -0300

    Adicionei o APP

--- src/main/java/br/anhembi/App.java
+++ src/main/java/br/anhembi/App.java
@@ -1,17 +1,70 @@
 package br.anhembi;
+import java.util.Scanner;
 
 /**
- * Hello world!
+ * Gerenciador de Fila!
  */
 public class App {
     private App() {
     }
 
     /**
-     * Says hello to the world.
-     * @param args The arguments of the program.
+     * Gerenciar filas.
+     * @author Guilherme Innocencio/ Guilherme.
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+      Scanner e = new Scanner(System.in);
+      Client cliente;
+      int num;
+      
+      ManageAttendance manage = new ManageAttendance(100);
+    	
+    	
+    	do {
+    		
+    		System.out.println("------------- ESCOLHA UMA OPÇÃO (1 à 5) -----------------------");
+        	System.out.println("|       1 – Chegada do cliente na agencia                     |");
+        	System.out.println("|       2 – Verificar quem é o próximo a ser atendido         |");
+        	System.out.println("|       3 – Atender um cliente                                |");
+        	System.out.println("|       4 – Exibir as filas (idoso e não idoso)               |");
+        	System.out.println("|       5 – Finalizar o programa                              |");
+        	System.out.println("---------------------------------------------------------------");
+            num = e.nextInt();
+            
+            switch(num) {
+            case 1: 
+            	System.out.println("Nome: ");
+            	String nome = e.next();
+            	System.out.println("Idade: ");
+            	int idade = e.nextInt();
+            	cliente = new Client(nome,idade);
+            	manage.addClient(cliente);
+            	break;
+            case 2: System.out.println(manage.numClients());
+            	
+            	
+            	
+            	break;
+           case 3: 
+        	   
+        	   
+        	   break;
+           case 4: System.out.println(manage.showQueues());
+        	   
+        	   
+        	   break;
+           case 5:
+        	   
+        	   
+        	
+        	}
+    		
+    	}while(num != 5);
+    	
+    	
+    	
+    	
+    e.close();  
+    	
     }
 }
